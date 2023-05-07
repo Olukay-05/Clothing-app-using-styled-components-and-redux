6ae3d521eca26eb6e49688b604bc5723fd15b56c
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-05-07 07:36:42 +0100

    Added Categories to Firestore

--- crwn-clothing/src/contexts/products.context.jsx
+++ crwn-clothing/src/contexts/products.context.jsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json"
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+// import SHOP_DATA from "../shop-data.js"
 
 
 export const ProductsContext = createContext({
@@ -8,9 +10,19 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
-    const value = { products }
+    const [products, setProducts] = useState([]);
+
+    // used to add categories to firebase and must run once.
+
+    // useEffect(()=>{
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // }, []);
+
+
+    const value = { products };
     return(
-        <ProductsContext.Provider value={ value }> { children } </ProductsContext.Provider>
+        <ProductsContext.Provider value={ value }> 
+            { children } 
+        </ProductsContext.Provider>
     )
 }
